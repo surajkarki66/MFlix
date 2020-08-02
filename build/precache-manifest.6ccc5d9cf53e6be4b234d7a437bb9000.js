1dfcd8e9d75b158bcdb0f58dd6227bf6aff6358d
Suraj Karki <suraj.karki500@gmail.com>
2020-08-02 12:01:10 +0545

    Initial Commit

---
+++ build/precache-manifest.6ccc5d9cf53e6be4b234d7a437bb9000.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09149907f9b1d1300ad2f9e251f848b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45584bedd290f2a3844",
+    "url": "/static/css/main.2637239d.chunk.css"
+  },
+  {
+    "revision": "1f4caf103cd0729aad5f",
+    "url": "/static/js/2.dc8c00c7.chunk.js"
+  },
+  {
+    "revision": "f45584bedd290f2a3844",
+    "url": "/static/js/main.de4ec786.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ebc18432c0a71cfd876a27933f91a4a",
+    "url": "/static/media/mongoleaf.0ebc1843.png"
+  },
+  {
+    "revision": "6c93bd2010905059e08c8b522adfc219",
+    "url": "/static/media/pixelatedLeaf.6c93bd20.svg"
+  }
+]);
