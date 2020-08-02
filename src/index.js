4107147cc90a0870addac3c6b3b00a5313ca3dcc
Suraj Karki <suraj.karki500@gmail.com>
2020-08-02 12:32:30 +0545

    Finished db-connection

--- src/index.js
+++ src/index.js
@@ -19,14 +19,13 @@ Ticket: Timeouts
 Please prevent the program from waiting indefinitely by setting the write
 concern timeout limit to 2500 milliseconds.
 */
-
 MongoClient.connect(
   process.env.MFLIX_DB_URI,
   // TODO: Connection Pooling
   // Set the poolSize to 50 connections.
   // TODO: Timeouts
   // Set the write timeout limit to 2500 milliseconds.
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
 )
   .catch(err => {
     console.error(err.stack)
